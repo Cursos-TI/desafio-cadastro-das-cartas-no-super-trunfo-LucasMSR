c0c7b28195bff2c62ca70b1da40bd3b0e2b4b05a
Lucas Matheus <lucasmdsr2001@gmail.com>
2025-08-06 23:49:39 +0000

    Criando minha primeira Carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,14 +9,14 @@ int main()
     char cidade [15] = ("Belo Horizonte");
     int populacao = 35500000;
     float area = 1550.90;
-    float pib = 750.35;
+    float pib = 750.355;
     int pontosTuristicos = 40;
 
     printf("Estado: %c\n" ,estado);
     printf("Código: %s\n" ,codigo);
     printf("Cidade: %s\n" ,cidade);
     printf("População %d\n" ,populacao);
-    printf("Área: %f\n" ,area);
+    printf("Área: %g\n" ,area);
     printf("PIB: %f\n" ,pib);
     printf("Pontos Turísticos: %d\n" ,pontosTuristicos);
 
