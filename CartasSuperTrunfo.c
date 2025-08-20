2f3d5463a69f2c7ebfbd6b97c1aaf149bb066623
Lucas Matheus <lucasmdsr2001@gmail.com>
2025-08-20 21:32:38 +0000

    Aprendendo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,29 +3,40 @@
 
 int main()
 {
-// Primeira Carta
-    char carta [10] = ("CARTA 1");
-    char estado = 'A';
-    char codigo [5] = ("A01");
-    char cidade [20] = ("Belo Horizonte");
-    int populacao = 2900000;
-    float area = 1550.90;
-    float pib = 750.355;
-    int pontosTuristicos = 40;
+// PRIMEIRA CARTA
 
-//  Dados da primeira carta
+char estado = 'A';
+char codigo[5] = ("A01");
+char cidade[20] = ("Belo Horizonte");
+int populacao = 1245000;
+float area = 1340.95;
+float pib = 15.325000;
+int pontosTuristicos = 16;
 
-    printf("Digite o número da sua carta: "); fgets(carta, 10, stdin);
+// SEGUNDA CARTA
 
-    printf("Carta 1 \n");
+char estado2 = 'B';
+char codigo2[5] = ("B02");
+char cidade2[20] = ("Fortaleza");
+int populacao2 = 321000;
+float area2 =1445.60;
+float pib2 = 17.67000;
+int pontosTuristicos2 = 25;
+
+
+
+
+// DADOS DA PRIMEIRA CARTA
+
+    printf("Digite o número da sua carta: ");
 
     printf("Estado: %c\n" ,estado); scanf("%c" ,&estado);
     
-    printf("Código da carta: %s\n" ,codigo); scanf("%s" ,&codigo);
+    printf("Código da carta: %s\n" ,codigo); fgets(codigo, 5, stdin);
     
     printf("Nome da cidade: %s\n" ,cidade); fgets(cidade, 20, stdin);
 
-    printf("População: %d\n"); scanf("%d", &populacao);
+    printf("População: %d\n" ,populacao); scanf("%d", &populacao);
 
     printf("Área: %f\n" ,area); scanf("%f" ,&area);
     
@@ -33,31 +44,15 @@ int main()
     
     printf("Pontos turísticos: %d\n" ,pontosTuristicos); scanf("%d" ,&pontosTuristicos);
 
+// DADOS DA SEGUNDA CARTA
 
-
-// Segunda Carta
-    char carta2 [10] = ("CARTA 2");
-    char estado2 = 'B';
-    char codigo2 [4] = "B02";
-    char cidade2 [15] = ("Fortaleza");
-    int populacao2 = 1060000;
-    float area2 = 4350.65;
-    float pib2 = 130.780;
-    int pontosTuristicos2 = 17;
-
-//  Dados da segunda carta
-
-    printf("Digite o número da sua carta: ");
-
-    printf("Carta 2 \n"); fgets(carta2, 10, stdin);
-    
     printf("Estado: %c\n" ,estado2); scanf("%c" ,&estado2);
 
-    printf("Código da carta: %s\n" ,codigo2); scanf("%s" ,&codigo2);
+    printf("Código da carta: %s\n" ,codigo2); fgets(codigo2, 5, stdin);;
     
     printf("Nome da cidade: %s\n" ,cidade2); fgets(cidade2, 15, stdin);
 
-    printf("População: %d\n"); scanf("%d", &populacao2);
+    printf("População: %d\n" ,populacao2); scanf("%d", &populacao2);
 
     printf("Área: %.2f\n" ,area2); scanf("%f" ,&area2);
     
@@ -66,5 +61,7 @@ int main()
     printf("Pontos turísticos: %d\n" ,pontosTuristicos2); scanf("%d" ,&pontosTuristicos2);
 
 
-    return 0;
+
+
+   return 0;
 }
